7cabcae67eaed0d2e3864f05e10468f19d18a592
Antoni <84175644+Atolio-cold@users.noreply.github.com>
2024-12-27 15:56:03 +0100

    Add files via upload

--- CrreateFirstDatabase.sql
+++ CrreateFirstDatabase.sql
@@ -6,3 +6,14 @@ EmpName varchar(255),
 City varchar(225),
 Salary int
 );
+
+insert into employee(ID, EmpName, City, Salary)
+values('1','Tom', 'Warsaw', '7000');
+
+select * from employee;
+
+insert into employee(ID, EmpName, City, Salary)
+values('2','Emma', 'London', '5000');
+
+
+select * from employee;
